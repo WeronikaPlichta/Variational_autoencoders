2b9968ff563fc4e9eb2a7d22da20fac761fbd426
wplichta <weronika.plichta23@gmail.com>
2023-06-01 15:06:53 +0200

    read signal

---
+++ loader.py
@@ -0,0 +1,40 @@
+import torch
+from torch.utils.data import DataLoader, Dataset
+class EEGSignalDataset(Dataset):
+    def __init__(self, data, gt, m=m, s=s, soft_label=True, train=True):
+        self.data = data
+        self.gt = gt
+        self.train = train
+        self.soft_label = soft_label
+        self.eps = 1e-7
+        if train:
+            self.index = resample_data(gt)
+        else:
+            self.index = [(i, j) for i in range(len(data)) for j in range(data[i].shape[1])]
+        for dt in self.data:
+            dt -= m
+            dt /= s + self.eps
+
+    def __getitem__(self, i):
+        i, j = self.index[i]
+        raw_data, label = self.data[i][:, max(0, j - opt.in_len + 1):j + 1], \
+            self.gt[i][:, j]
+
+        pad = opt.in_len - raw_data.shape[1]
+        if pad:
+            raw_data = np.pad(raw_data, ((0, 0), (pad, 0)), 'constant', constant_values=0)
+
+        raw_data, label = torch.from_numpy(raw_data.astype(np.float32)), \
+            torch.from_numpy(label.astype(np.float32))
+        if self.soft_label:
+            label[label < .02] = .02
+        return raw_data, label
+
+    def __len__(self):
+        return len(self.index)
+
+
+dataset = EEGSignalDataset(trainset, gt)
+dataloader = DataLoader(dataset, batch_size=opt.batch_size, \
+                        num_workers=opt.n_cpu, shuffle=True)
+print(len(dataset))
--- preprocessing.py
+++ preprocessing.py
@@ -10,13 +10,47 @@ file_list_abnormal = os.listdir(filepath_abnormal)
 file_list_normal = os.listdir(filepath_normal)
 
 
-def load_signal(file):
+def load_signal(file, i):
     eeg_raw = mne.io.read_raw_edf(file)
-    mont = mne.channels.make_standard_montage('standard_1020')
-    eeg_mont = eeg_raw.copy().set_montage(mont)
+    channels = eeg_raw.ch_names[0:20]
+    eeg_data = eeg_raw.load_data()
+    eeg_data.set_eeg_reference('average', projection=True)
+    eeg_data.apply_proj()
+    eeg_filter_1 = eeg_data.filter(l_freq=0.1, h_freq=None)
+    eeg_filter_2 = eeg_filter_1.notch_filter(freqs=60)
+    signal = eeg_filter_2.resample(sfreq=100)
+    df = signal.to_data_frame(picks=channels)
+    matrix = df.to_numpy()
+    np.save(f"data{i}", matrix)
+    return matrix
 
-    return signal
+
+def prepare_data(matrix):
 
 
-def prepare_data(signal):
     return tensor
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
