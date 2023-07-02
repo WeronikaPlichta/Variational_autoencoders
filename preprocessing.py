5fb65ae3306f080c5030fe3bc25cb9ba019cc379
wplichta <weronika.plichta23@gmail.com>
2023-07-02 23:54:23 +0200

    downsample tf maps

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,5 @@
 import os
-import scipy as sig
+import scipy.signal as ss
 import mne
 import numpy as np
 import matplotlib.pyplot as plt
@@ -25,13 +25,13 @@ def prepare_dataset(matrix):
     size = matrix.shape
     print("MATRIX SHAPE:", matrix.shape)
     epochs = size[0]//600
-    new_signal = np.zeros((600, 40, 20, epochs))
+    new_signal = np.zeros((40, 60, 20, epochs))
     # new_signal = np.zeros((600, 20, epochs))
-    freq = np.linspace(1, 40, 100)
+    freq = np.linspace(1, 40, 40)
     w = 7
     widths = w * 100 / (2 * freq * np.pi)
     for i in range(epochs):
         for j in range(20):
-            P = sig.cwt(matrix[i * 600:(i * 600) + 600, j], sig.morlet2, widths, w=w)
-            new_signal[:, :, j, i] = P
+            P = ss.cwt(matrix[i * 600:(i * 600) + 600, j], ss.morlet2, widths, w=w)
+            new_signal[:, :, j, i] = np.abs(P[:, ::10])
     return new_signal
